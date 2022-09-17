a0fae2ee8a501a91c44b26261124587d52d789d1
Autsada <autsada.t@gmail.com>
2022-09-17 18:43:29 +0700

    feat:update port config

--- src/app.ts
+++ src/app.ts
@@ -3,26 +3,11 @@ import dotenv from 'dotenv'
 dotenv.config({ path: path.join(__dirname, '../.env') })
 import express from 'express'
 import cors from 'cors'
-import https from 'https'
 import http from 'http'
-import fs from 'fs'
 
 import * as router from './routes'
-import type { Environment, EnvConfig } from './types'
 
-const { PORT, NODE_ENV } = process.env
-
-const configurations: Record<Environment, EnvConfig> = {
-  production: { ssl: true, port: Number(PORT) || 8000, hostname: '' },
-  development: {
-    ssl: false,
-    port: Number(PORT) || 8000,
-    hostname: 'localhost',
-  },
-}
-
-const environment = (NODE_ENV || 'production') as Environment
-const config: EnvConfig = configurations[environment]
+const { PORT } = process.env
 
 const app = express()
 app.use(express.json()) // for parsing application/json
@@ -32,30 +17,9 @@ app.use(cors())
 app.use('/profiles', router.profilesRouter)
 app.use('/wallet', router.walletRouter)
 
-// Create the HTTPS or HTTP server, per configuration
-let httpServer: https.Server | http.Server
-if (config.ssl) {
-  // Assumes certificates are in a .ssl folder off of the package root.
-  // Make sure these files are secured.
-  httpServer = https.createServer(
-    {
-      key: fs.readFileSync(
-        path.join(__dirname, `../ssl/${environment}/server.key`)
-      ),
-      cert: fs.readFileSync(
-        path.join(__dirname, `../ssl/${environment}/server.crt`)
-      ),
-    },
+// Create the HTTP server
+const httpServer = http.createServer(app)
 
-    app
-  )
-} else {
-  httpServer = http.createServer(app)
-}
-
-httpServer.listen({ port: config.port }, () => {
-  console.log(
-    'Server ready at',
-    `http${config.ssl ? 's' : ''}://${config.hostname}:${config.port}`
-  )
+httpServer.listen({ port: PORT || 8000 }, () => {
+  console.log(`Server ready at port: ${PORT}`)
 })
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,1 @@
 export type Environment = 'production' | 'development'
-
-export type EnvConfig = {
-  ssl: boolean
-  port: number
-  hostname: string
-}
