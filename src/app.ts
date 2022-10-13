14b65bf824da9ad0b43e891698dbf87674db3289
Autsada T <autsada@Autsadas-MacBook-Pro.local>
2022-10-13 15:21:30 +0700

    add firebase

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
-import path from 'path'
-import dotenv from 'dotenv'
-dotenv.config({ path: path.join(__dirname, '../.env') })
-import express from 'express'
-import cors from 'cors'
-import http from 'http'
+import path from "path"
+import dotenv from "dotenv"
+dotenv.config({ path: path.join(__dirname, "../.env") })
+import express from "express"
+import cors from "cors"
+import http from "http"
 
-import * as router from './routes'
+import "./config/firebase"
+import * as router from "./routes"
 
 const { PORT } = process.env
 
@@ -14,9 +15,9 @@ app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 app.use(cors())
 
-app.use('/wallet', router.walletRouter)
-app.use('/profiles', router.profilesRouter)
-app.use('/publishes', router.publishesRouter)
+app.use("/wallet", router.walletRouter)
+app.use("/profiles", router.profilesRouter)
+app.use("/publishes", router.publishesRouter)
 
 // Create the HTTP server
 const httpServer = http.createServer(app)
---
+++ src/lib/firebase.ts
@@ -0,0 +1,43 @@
+import { firestore } from "firebase-admin"
+
+import { db } from "../config/firebase"
+
+type Args<T = Record<string, any>> = {
+  collectionName: string
+  docId: string
+  data: T
+  fieldName: string
+  fieldValue: any
+}
+
+export function snapshotToDoc<T extends Record<string, any>>(
+  snapshot: firestore.DocumentSnapshot<firestore.DocumentData>
+) {
+  const data = snapshot.data() as T & {
+    createdAt: firestore.Timestamp
+    updatedAt?: firestore.Timestamp
+  }
+
+  const createdAt = data?.createdAt ? data.createdAt.toDate().toString() : null
+  const updatedAt = data?.updatedAt ? data.updatedAt.toDate().toString() : null
+
+  const doc: T = {
+    ...data,
+    id: snapshot.id,
+    createdAt,
+    updatedAt,
+  }
+
+  return doc
+}
+
+export async function getDocById<T extends Record<string, any>>({
+  collectionName,
+  docId,
+}: Pick<Args, "collectionName" | "docId">) {
+  const snapshot = await db.collection(collectionName).doc(docId).get()
+
+  if (!snapshot.exists) return null
+
+  return snapshotToDoc<T>(snapshot)
+}
