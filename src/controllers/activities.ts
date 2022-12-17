02ef3b4a8e96bf989fb166d969edf46432b5291f
Autsada T <autsada@gmail.com>
2022-12-17 15:44:51 +0700

    add check before writing doc to Firestore

--- src/controllers/activities.ts
+++ src/controllers/activities.ts
@@ -1,8 +1,11 @@
 import type { Request, Response } from "express"
+import axios from "axios"
 
 import { activitiesCollection } from "../config/firebase"
 import { updateDocById } from "../lib/firebase"
 import type { WebHookRequestBody, AddressActivity } from "../types"
+
+const { PUBLIC_API_URL } = process.env
 
 export async function updateActivity(req: Request, res: Response) {
   try {
@@ -14,35 +17,67 @@ export async function updateActivity(req: Request, res: Response) {
       const fromAddress = activity.fromAddress.toLowerCase()
       const toAddress = activity.toAddress.toLowerCase()
 
-      // Update sender's activity
-      await updateDocById<Omit<AddressActivity, "id">>({
-        collectionName: activitiesCollection,
-        docId: fromAddress,
-        data: {
-          event: activity.category,
-          fromAddress,
-          toAddress,
-          value: activity.value,
-          isAcknowledged: false,
-        },
-      })
+      // Get the sender account.
+      const sender = await getAccount(fromAddress)
+      if (sender) {
+        // Update sender's activity
+        await updateDocById<Omit<AddressActivity, "id">>({
+          collectionName: activitiesCollection,
+          docId: fromAddress,
+          data: {
+            event: activity.category,
+            fromAddress,
+            toAddress,
+            value: activity.value,
+            isAcknowledged: false,
+          },
+        })
+      }
 
-      // Update receiver's activity
-      await updateDocById<Omit<AddressActivity, "id">>({
-        collectionName: activitiesCollection,
-        docId: toAddress,
-        data: {
-          event: activity.category,
-          fromAddress,
-          toAddress,
-          value: activity.value,
-          isAcknowledged: false,
-        },
-      })
+      // Get the receiver account.
+      const receiver = await getAccount(toAddress)
+      if (receiver) {
+        // Update receiver's activity
+        await updateDocById<Omit<AddressActivity, "id">>({
+          collectionName: activitiesCollection,
+          docId: toAddress,
+          data: {
+            event: activity.category,
+            fromAddress,
+            toAddress,
+            value: activity.value,
+            isAcknowledged: false,
+          },
+        })
+      }
     }
 
     res.status(200).end()
   } catch (error) {
     res.status(500).send((error as any).message)
   }
+}
+
+async function getAccount(address: string): Promise<{ id: number } | null> {
+  const ressult = await axios({
+    url: `${PUBLIC_API_URL}/graphql`,
+    method: "post",
+    headers: {
+      "content-type": "application/json",
+    },
+    data: {
+      query: `
+        query GetAccount($address: String!) {
+          getAccount(address: $address) {
+            id
+          }
+        }
+      `,
+      variables: {
+        address,
+      },
+    },
+  })
+
+  return ressult.data.data?.getAccount
 }
